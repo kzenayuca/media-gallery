ec04c7a4b9d3b5f3c0a6143a4105f8eee9ab5ee9
kzenayuca <kzenayuca@unsa.edu.pe>
2026-09-07 19:43:29 -0500

    Estructura inicial

---
+++ src/test/java/com/media_gallery/MediaGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media_gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
